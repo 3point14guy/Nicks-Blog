bfc391f3ca3820ae3d5626e4d6405a01fbd95fc9
3poitn14guy <nicholas.piaskoski@gmail.com>
2018-02-13 08:44:20 -0500

    final day of project

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def user_posts
-    @user = User.find(params[:id])
+    @user = User.find_by(username: params[:name])
   end
 
   # GET /posts/1
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
 
-  get 'user_posts' => 'posts#user_posts'
+  get '/:name' => 'posts#user_posts', as: :user_posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
